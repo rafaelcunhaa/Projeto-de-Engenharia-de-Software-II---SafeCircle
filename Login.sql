cc990f8fd84ab7a9668b317100f15557b0c00b81
Rafael da Cunha <136820942+rafaelcunhaa@users.noreply.github.com>
2025-05-08 17:30:06 -0300

    Add files via upload

---
+++ Login.sql
@@ -0,0 +1,14 @@
+CREATE DATABASE SafeCircle
+USE SafeCircle
+
+CREATE TABLE Usuario (
+	id_user INT PRIMARY KEY IDENTITY,
+	email VARCHAR(50) NOT NULL,
+	senha VARCHAR(50) NOT NULL,
+	dat_nac DATE NOT NULL,
+	telefone INT UNIQUE NOT NULL,
+	cpf VARCHAR(11) UNIQUE NOT NULL,
+	rg VARCHAR(9) UNIQUE NOT NULL,
+	ind_front VARBINARY(MAX), -- foto da indentidade 
+	ind_back VARBINARY(MAX)  -- foto do outro lado da indentidade
+);
