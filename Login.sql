f3a31f974c80d96563c85a917f7077b92100ab1c
Rafael da Cunha <136820942+rafaelcunhaa@users.noreply.github.com>
2025-05-09 21:32:33 -0300

    Update Login.sql

--- Login.sql
+++ Login.sql
@@ -3,6 +3,7 @@ USE SafeCircle
 
 CREATE TABLE Usuario (
 	id_user INT PRIMARY KEY IDENTITY,
+	nome VARCHAR(50) NOT NULL
 	email VARCHAR(50) NOT NULL,
 	senha VARCHAR(50) NOT NULL,
 	dat_nac DATE NOT NULL,
